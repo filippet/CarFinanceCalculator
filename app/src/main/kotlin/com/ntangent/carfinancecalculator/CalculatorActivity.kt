8cc51328f8163c6a518fd32b21c62a5139980cf6
Filip Petrovic <filippet@gmail.com>
2017-04-08 11:39:18 -0700

    Kotlin

---
+++ app/src/main/kotlin/com/ntangent/carfinancecalculator/CalculatorActivity.kt
@@ -0,0 +1,12 @@
+package com.ntangent.carfinancecalculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CalculatorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+    }
+}
